e346f6f70ff306fd442fe25a37c5878d99b329b9
hmarvikhoras <hmarvikhoras@mun.ca>
2018-06-05 20:39:35 -0230

    some improvements on Min/Max plots

--- plotMinMax.py
+++ plotMinMax.py
@@ -20,25 +20,39 @@ def MinMaxPlot(lyst):
     df2=clean_data(df2)
     
     days=range(0,len(df))
-    plt.subplots(figsize=(10,7))
+    plt.subplots(figsize=(15,15))
+    
     
     temp_Max = df.iloc[:,[4]]
     plt.subplot(3,1,1)
     ax2=plt.plot(days, temp_Max,label="Maximum Temp")
     temp_Min= df.iloc[:,[5]]
     ax1=plt.plot(days, temp_Min,label="Minimum Temp")
-    plt.xticks([0,30,60,90,120,150,180,210,240,270,300,330,360]),([r'$Jan$',r'$Feb$',r'$Mar$',r'$Apr$',r'$May$',r'$Jun$',r'$July2$',r'$Aug3$',r'$Sep4$',r'$Oct5$',r'$Nov6$',r'$Dec$'])
-    plt.xlim([0,360])
-    plt.legend()
-        
+    plt.xticks([0,30,60,90,120,150,180,210,240,270,300,330,360])
+    plt.xlim([0,366])
+    ax = plt.gca()
+    ax.set_xlabel('Days', fontsize=15)
+    ax.set_ylabel('Temperature', fontsize=15)
+    plt.title('Annual cycle of Min and Max daily temperatures of '+lyst[0]+' in '+str(lyst[1]), color="black", fontsize=18)
+    plt.legend(loc='upper right')
+    plt.tight_layout(True)
+    plt.grid(True)
+    
     plt.subplot(3,1,2)
     temp_Max1 = df1.iloc[:,[4]]
     temp_Min1= df1.iloc[:,[5]]
     days2 = range(0, len(df1))
     plt.plot(days2, temp_Max1,label="Maximum Temp")
     plt.plot(days2, temp_Min1,label="Minimum Temp")
-    plt.xticks([0,30,60,90,120,150,180,210,240,270,300,330,360]),([r'$Jan$',r'$Feb$',r'$Mar$',r'$Apr$',r'$May$',r'$Jun$',r'$July2$',r'$Aug3$',r'$Sep4$',r'$Oct5$',r'$Nov6$',r'$Dec$'])
-    plt.legend()
+    ax = plt.gca()
+    plt.xlim([0,366])
+    plt.xticks([0,30,60,90,120,150,180,210,240,270,300,330,360])
+    ax.set_xlabel('Days', fontsize=15)
+    ax.set_ylabel('Temperature', fontsize=15)
+    plt.title('Annual cycle of Min and Max daily temperatures of '+lyst[2]+' in '+str(lyst[3]), color="black", fontsize=18)
+    plt.legend(loc='upper right')
+    plt.grid(True)
+    
     
     plt.subplot(3,1,3)
     temp_Max1 = df2.iloc[:,[4]]
@@ -46,10 +60,14 @@ def MinMaxPlot(lyst):
     days3 = range(0, len(df2))
     plt.plot(days3, temp_Max1,label="Maximum Temp")
     plt.plot(days3, temp_Min1,label="Minimum Temp")
-    plt.xticks([0,30,60,90,120,150,180,210,240,270,300,330,360]),([rr'$Jan$',r'$Feb$',r'$Mar$',r'$Apr$',r'$May$',r'$Jun$',r'$July2$',r'$Aug3$',r'$Sep4$',r'$Oct5$',r'$Nov6$',r'$Dec$'])
-    plt.legend()
-    plt.xlim([0,360])
+    ax = plt.gca()
+    ax.set_xlabel('Days', fontsize=15)
+    ax.set_ylabel('Temperature', fontsize=15)
+    plt.title('Annual cycle of Min and Max daily temperatures of '+lyst[4]+' in '+str(lyst[5]), color="black", fontsize=18)
+    plt.legend(loc='upper right')
+    plt.xlim([0,366])
+    plt.xticks([0,30,60,90,120,150,180,210,240,270,300,330,360])
     plt.tight_layout()
+    plt.grid(True)
     plt.show()
-    #print(len(df)) 
-    
+   
