5578130958c8321888a2b51b8018ca661347c58a
cjconway <cjconway@mun.ca>
2018-06-05 21:52:58 -0230

    Added the #! to top of the file, used station id 3932 and 2016 as an example, and removed the RC characters found in the file from when textedit was used to create the file.

--- modifiedDownloadData.py
+++ modifiedDownloadData.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import urllib.request
 import sys
@@ -8,15 +10,17 @@ def download(stationid, fromYear,toYear):
     print("Data of station id {} for year {}".format(stationid, fromYear))
     data_frame2=[]
     data_frame1=pd.DataFrame()
-    print(data_frame1)
+    #print(data_frame1)
     for year in range(fromYear,toYear+1):
         fname = "{}_{}_t.csv".format(stationid, year)
-        url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID="+str(stationid)+"&Year="+str(year)+"&Month=8&Day=1&timeframe=2&submit=Download+Data"
+        url = ("http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID="+str(stationid)+"&Year="+str(year)+"&Month=8&Day=1&timeframe=2&submit=Download+Data")
         urllib.request.urlretrieve(url, fname)
         data_frame2 = pd.read_csv(fname, skiprows=24, sep=",")
         data_frame1=pd.concat([data_frame2, data_frame1], join='outer')
         os.remove(fname)
     result = data_frame1[['Date/Time','Year','Month','Day','Max Temp (°C)','Min Temp (°C)']]
-    print(result)
+    #print(result)
     result.to_csv("temperatures.csv", index=False)
     return result
+
+download(3932, 2016, 2017)
