ac60decf12f643442cf091f161ee020292ebe834
cjconway <cjconway@mun.ca>
2018-06-06 21:46:17 -0230

    Finished off the code for gdd calculations

--- gdd.py
+++ gdd.py
@@ -7,12 +7,23 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('data', action="store", type=str)
 parser.add_argument('tbase', action="store", type=int)
-parser.add_argument('tupper', action="store")
+parser.add_argument('tupper', action="store", type=int)
 
 args = parser.parse_args()
 
 data = pd.read_csv(args.data)
 data = data.fillna(0)
 
+i = 0
+gddtable = []
 for index, row in data.iterrows():
-    print(row.loc['Max Temp (°C)'])
+    gdd = (((data.iloc[i][4] - data.iloc[i][5])/2) - args.tbase)
+    if gdd <= 0:
+        gdd = 0
+    elif gdd >= args.tupper:
+        gdd = args.tupper
+    gddtable.append(gdd)
+    i += 1
+
+totalgdd = sum(gddtable)
+print(totalgdd)
